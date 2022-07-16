7e29287d8f45b8b33113a1b051d4c1c718f975e9
Sumit Agrawal <gophersumit@gmail.com>
2022-07-16 20:04:43 +0530

    simple api endpoints

---
+++ cmd/api/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 8080, "port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "environment")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{cfg, logger}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 30,
+	}
+
+	logger.Printf("Starting %s server on port %d", cfg.env, cfg.port)
+	logger.Fatal(srv.ListenAndServe())
+}
