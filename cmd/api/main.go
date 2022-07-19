a0720a20ef9eaad7cdb32102a79f33e2b19c4c1b
Sumit Agrawal <gophersumit@gmail.com>
2022-07-19 12:20:23 +0530

    db connection setup for postgres

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +18,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 type application struct {
 	config config
@@ -25,9 +32,23 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "environment")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
+
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	// Defer a call to db.Close() so that the connection pool is closed before the
+	// main() function exits.
+	defer db.Close()
+
+	// Also log a message to say that the connection pool has been successfully
+	// established.
+	logger.Printf("database connection pool established")
 
 	app := &application{cfg, logger}
 
@@ -44,4 +65,22 @@ func main() {
 
 	logger.Printf("Starting %s server on port %d", cfg.env, cfg.port)
 	logger.Fatal(srv.ListenAndServe())
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the sql.DB connection pool.
+	return db, nil
 }
